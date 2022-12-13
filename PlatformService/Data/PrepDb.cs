86ffb9b4bd8290050dfd4b6b8ed980808d686bc6
Gennadios <egon.sch@yandex.ru>
2022-12-13 21:51:06 +0400

    add PrepDb to populate sample data

---
+++ PlatformService/Data/PrepDb.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using PlatformService.Models;
+using System.Linq;
+
+namespace PlatformService.Data
+{
+    public static class PrepDb
+    {
+        public static void PopulateInMemoryDb(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+            }
+        }
+
+        private static void SeedData(AppDbContext dbContext)
+        {
+            if (!dbContext.Platforms.Any())
+            {
+                dbContext.Platforms.AddRange(
+                    new Platform() { Name = "dotnet", Publisher = "Microsoft", Cost = "Free" },
+                    new Platform() { Name = "SQL Server", Publisher = "Microsoft", Cost = "Free" },
+                    new Platform() { Name = "Docker", Publisher = "Docker, Inc.", Cost = "Free" });
+            }
+        }
+    }
+}
