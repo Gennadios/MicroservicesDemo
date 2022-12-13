60a5e9fb71e33344ed9018c0e3ca4bd6160754f6
Gennadios <egon.sch@yandex.ru>
2022-12-13 21:08:00 +0400

    add db context class

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,6 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMemoryDB"));
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
